28cd66b7f645013fe38c76bd8c03486797e1b9b3
Krish-Anand-dev <krishanand.dev@gmail.com>
2025-08-19 03:42:39 +0530

    Added variables example with console.table

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const final = "hello";
+let word = "world";
+let city = "delhi"; // we can also use "var" but it is not recommended
+let AGE; // when we don't assign any value, it is undefined
+
+console.table([final, word, city, AGE]);
+AGE = 40;
+console.table([final, word, city, AGE]);
